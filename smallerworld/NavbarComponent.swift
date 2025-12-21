a05c26dafa55bd3579fd7983eba1146e7450eb17
kai <hulloitskai@gmail.com>
2025-12-21 03:37:07 -0500

    Initial commit

---
+++ smallerworld/NavbarComponent.swift
@@ -0,0 +1,23 @@
+import UIKit
+import HotwireNative
+
+struct NavbarProps: Decodable {
+    let items: [Item]
+
+    struct Item: Decodable {
+        let id: String
+        let title: String
+        let icon: String? // optional, SF Symbol name
+    }
+}
+
+@MainActor
+final class NavbarComponent: BridgeComponent {
+//    override class var name: String { "navbar" }
+
+//    @Published var props = NavbarProps(items: [])
+//
+//    func update(with props: NavbarMenuProps) {
+//        self.props = props
+//    }
+}
