81d2c5f6c1b4d28c93ad5b719da7910085b05991
kai <1376678+hulloitskai@users.noreply.github.com>
2025-12-25 01:48:42 -0500

    Enable navigating from applink

--- smallerworld/SceneDelegate.swift
+++ smallerworld/SceneDelegate.swift
@@ -14,15 +14,35 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         startLocation: rootURL,
     ))
 
+    // Triggers from a cold start
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         window?.rootViewController = navigator.rootViewController
         navigator.start()
+        
+        if let userActivity = connectionOptions.userActivities.first {
+            if userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+               let incomingURL = userActivity.webpageURL {
+                navigator.route(incomingURL)
+            }
+        }
         
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         // guard let _ = (scene as? UIWindowScene) else { return }
     }
+   
+    // Triggers when app is running in the background
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        if userActivity.activityType != NSUserActivityTypeBrowsingWeb {
+            return
+        }
+        guard let incomingURL = userActivity.webpageURL else {
+            return
+        }
+        navigator.route(incomingURL)
+    }
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
