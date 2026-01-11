181e676a2d53e53f0ab97929625b1327ccf3a4ff
kai <1376678+hulloitskai@users.noreply.github.com>
2026-01-11 15:57:30 -0500

    Wait for installation ID to be available

--- smallerworld/InstallationID.swift
+++ smallerworld/InstallationID.swift
@@ -10,15 +10,21 @@ final class InstallationID {
 
   private init() {}
 
-  func get() -> String {
-    UIDevice.current.identifierForVendor?.uuidString ?? ""
+  func get() async -> String {
+    let sleepNanos = UInt64(1_000_000_000)
+    while true {
+      if let id = UIDevice.current.identifierForVendor?.uuidString {
+        return id
+      }
+      try? await Task.sleep(nanoseconds: sleepNanos)
+    }
   }
 
-  func setDefaultCookie(completionHandler: (() -> Void)? = nil) {
+  func setDefaultCookie() async {
     let components = URLComponents(url: AppConstants.rootURL, resolvingAgainstBaseURL: true)!
     var properties: [HTTPCookiePropertyKey: Any] = [
       .name: cookie_name,
-      .value: get(),
+      .value: await get(),
       .domain: components.host!,
       .path: "/",  // A path of "/" makes it available to all paths on the domain
       .secure: components.scheme == "https",
@@ -27,6 +33,10 @@ final class InstallationID {
       properties[.port] = port
     }
     let cookie = HTTPCookie(properties: properties)!
-    WKWebsiteDataStore.default().httpCookieStore.setCookie(cookie, completionHandler: completionHandler)
+    await withCheckedContinuation { continuation in
+      WKWebsiteDataStore.default().httpCookieStore.setCookie(cookie) {
+        continuation.resume()
+      }
+    }
   }
 }
