33f00a4fc347f33ed0cb85f263ab1eaa68e767b5
kai <1376678+hulloitskai@users.noreply.github.com>
2026-06-06 11:01:53 -0400

    Update path-configuration

--- smallerworld/AppDelegate.swift
+++ smallerworld/AppDelegate.swift
@@ -60,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
+
     // MARK: Helpers
 
     private func styleApplication() {
@@ -81,8 +81,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             #endif
             return webView
         }
-//        Hotwire.config.showDoneButtonOnModals = true
-//        Hotwire.config.animateReplaceActions = true
+        Hotwire.config.animateReplaceActions = true
 
         // == Bridge components
         var components = Bridgework.coreComponents
